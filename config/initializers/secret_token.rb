a6bd021655b06837965dba6655d5a32162cb4eee
Jesse Pierce <pierce.jesse07@gmail.com>
2013-03-17 17:44:33 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labelwhores::Application.config.secret_token = '165ff9eaeaa0d0cc9c9d1f9cf63477b0c342ec7a02122c09ea5bdf2f4c17f3fe370b7c888bde7605c703e7f1a2bdcb78feb58b935f9fb5d9367115ca97d86dc6'
